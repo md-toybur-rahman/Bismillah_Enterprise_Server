1d546298c537291a6470632518e4c528a3cef86a
Bismillah-Enterprise <bismillah786e@gmail.com>
2025-07-12 04:47:38 +0600

    add all from old

--- index.js
+++ index.js
@@ -33,6 +33,10 @@ async function run() {
         const shopLocationCollection = client.db('Bismillah_Enterprise').collection('shop_location');
         const userRequestCollection = client.db('Bismillah_Enterprise').collection('user_request');
         const shopCodeCollection = client.db('Bismillah_Enterprise').collection('shop_code');
+        const additionalMovementRequestCollection = client.db('Bismillah_Enterprise').collection('additional_movement_request');
+        const shopTransectionsCollection = client.db('Bismillah_Enterprise').collection('shop_transections');
+        const shopTransectionsSummaryCollection = client.db('Bismillah_Enterprise').collection('shop_transections_summary');
+        const noticePanelCollection = client.db('Bismillah_Enterprise').collection('notice_panel');
 
         app.get("/shop_code/:id", async (req, res) => {
             const id = req.params.id;
@@ -53,6 +57,24 @@ async function run() {
 
             try {
                 const result = await shopCodeCollection.updateOne(filter, shopCode, options);
+                res.send(result);
+            } catch (err) {
+                res.status(500).send({ error: 'Update failed', details: err });
+            }
+        });
+        app.put('/additional_request_approve/:uid', async (req, res) => {
+            const uid = req.params.uid;
+            const filter = { uid: uid };
+            const options = { upsert: true };
+            const updatedStatus = req.body;
+            const movementStatus = {
+                $set: {
+                    additional_movement_status: updatedStatus.additional_movement_status
+                }
+            };
+
+            try {
+                const result = await staffsCollection.updateOne(filter, movementStatus, options);
                 res.send(result);
             } catch (err) {
                 res.status(500).send({ error: 'Update failed', details: err });
@@ -97,7 +119,27 @@ async function run() {
                 res.status(500).send({ error: "Failed to insert user request" });
             }
         })
+        app.delete('/staff/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const result = await staffsCollection.deleteOne(filter);
+            res.send(result);
+        })
 
+        // app.get('/get_network_ip', (req, res) => {
+        //     const interfaces = os.networkInterfaces();
+        //     let localIp = null;
+
+        //     for (let name in interfaces) {
+        //         for (let iface of interfaces[name]) {
+        //             if (iface.family === 'IPv4' && !iface.internal) {
+        //                 localIp = iface.address;
+        //             }
+        //         }
+        //     }
+
+        //     res.json({ ip: localIp || 'Not found' });
+        // });
         app.get('/user_request_uid/:uid', async (req, res) => {
             const uid = req.params.uid;
 
@@ -122,6 +164,26 @@ async function run() {
                 res.status(500).send({ error: "Failed to insert user request" });
             }
         });
+        app.get('/additional_movement_request', async (req, res) => {
+            const result = await additionalMovementRequestCollection.find().toArray();
+            res.send(result);
+        })
+        app.post('/additional_movement_request', async (req, res) => {
+            const movementData = req.body;
+            try {
+                const result = await additionalMovementRequestCollection.insertOne(movementData);
+                res.send(result);
+            } catch (err) {
+                res.status(500).send({ error: "Failed to insert request" });
+            }
+        });
+        app.delete('/additional_movement_request/:uid', async (req, res) => {
+            const uid = req.params.uid;
+            const filter = { uid: uid };
+            const result = await additionalMovementRequestCollection.deleteOne(filter);
+            res.send(result);
+        });
+
 
         app.get('/user_request', async (req, res) => {
             const user = await userRequestCollection.find().toArray();
@@ -139,6 +201,25 @@ async function run() {
             res.send(result);
         })
 
+
+        // app.put('/set_ip/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const filter = { _id: new ObjectId(id) };
+        //     const options = { upsert: true };
+        //     const updatedIP = req.body;
+        //     const wifiIP = {
+        //         $set: {
+        //             wifi_ip: updatedIP.wifi_ip
+        //         }
+        //     };
+
+        //     try {
+        //         const result = await wifiIpCollection.updateOne(filter, wifiIP, options);
+        //         res.send(result);
+        //     } catch (err) {
+        //         res.status(500).send({ error: 'Update failed', details: err });
+        //     }
+        // });
         app.put('/staffs_daily_time/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
@@ -148,7 +229,8 @@ async function run() {
             if (updatedTime.name == 'today_enter1_time') {
                 attendance = {
                     $set: {
-                        today_enter1_time: updatedTime.clickedTime
+                        today_enter1_time: updatedTime.clickedTime,
+                        today_date: updatedTime.today_date
                     }
                 };
             }
@@ -181,6 +263,56 @@ async function run() {
                 res.status(500).send({ error: 'Update failed', details: err });
             }
         });
+        app.put('/additional_movements/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updatedTime = req.body;
+            let attendance
+            if (updatedTime.name == 'additional_enter_time') {
+                attendance = {
+                    $set: {
+                        additional_enter_time: updatedTime.clickedTime
+                    }
+                };
+            }
+            else if (updatedTime.name == 'additional_exit_time') {
+                attendance = {
+                    $set: {
+                        additional_exit_time: updatedTime.clickedTime
+                    }
+                };
+            }
+
+            try {
+                const result = await staffsCollection.updateOne(filter, attendance, options);
+                res.send(result);
+            } catch (err) {
+                res.status(500).send({ error: 'Update failed', details: err });
+            }
+        });
+
+        // app.put('/reset_time/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const updatedTime = req.body
+        //     const filter = { _id: new ObjectId(id) };
+        //     const updateDoc = {
+        //         $set: {
+        //             today_enter1_time: updatedTime.enter1_time,
+        //             today_exit1_time: updatedTime.exit1_time,
+        //             today_enter2_time: updatedTime.enter2_time,
+        //             today_exit2_time: updatedTime.exit2_time
+        //         }
+        //     };
+
+        //     try {
+        //         const result = await staffsCollection.updateOne(filter, updateDoc);
+        //         res.status(200).send(result);
+        //     } catch (err) {
+        //         console.error('Update error:', err);
+        //         res.status(500).send({ error: 'Update failed', details: err });
+        //     }
+        // });
 
         // code with calculate and set daily data
         app.put('/reset_time/:id', async (req, res) => {
@@ -288,6 +420,7 @@ async function run() {
                 res.json({
                     latitude: location.latitude,
                     longitude: location.longitude,
+                    shop_range: location.shop_range,
                 });
             } catch (error) {
                 console.error('GET /shop_location error:', error);
@@ -320,6 +453,274 @@ async function run() {
             }
         });
 
+        app.put('/submit_work_time/:id', async (req, res) => {
+            const id = req.params.id;
+            const bodyData = req.body;
+            const todaySummary = {
+                current_date: bodyData.currentDate,
+                current_day_name: bodyData.currentDayName,
+                today_enter1_time: bodyData.today_enter1_time,
+                today_exit1_time: bodyData.today_exit1_time,
+                today_enter2_time: bodyData.today_enter2_time,
+                today_exit2_time: bodyData.today_exit2_time,
+                total_hour: bodyData.total_hour,
+                total_minute: bodyData.total_minute,
+                total_earn: bodyData.total_earn,
+                additional_movement_hour: bodyData.additional_movement_hour,
+                additional_movement_minute: bodyData.additional_movement_minute
+
+            }
+            const filter = { _id: new ObjectId(id) };
+
+            try {
+                // Update database: Push daily data & reset today's values
+                const updateDoc = {
+                    $push: { current_month_details: todaySummary },
+                    $set: {
+                        total_working_hour: bodyData.total_working_hour,
+                        total_working_minute: bodyData.total_working_minute,
+                        total_income: bodyData.total_income,
+                        today_enter1_time: '',
+                        today_exit1_time: '',
+                        today_enter2_time: '',
+                        today_exit2_time: '',
+                        additional_enter_time: '',
+                        additional_exit_time: '',
+                        additional_movement_hour: 0,
+                        additional_movement_minute: 0
+                    }
+                };
+                const ErrorDoc = {
+                    $set: {
+                        today_enter1_time: '',
+                        today_exit1_time: '',
+                        today_enter2_time: '',
+                        today_exit2_time: '',
+                        additional_enter_time: '',
+                        additional_exit_time: '',
+                        additional_movement_hour: 0,
+                        additional_movement_minute: 0
+                    }
+                }
+                if (bodyData.today_exit1_time === '' || bodyData.today_exit2_time === '') {
+                    const result = await staffsCollection.updateOne(filter, ErrorDoc);
+                    res.send(result);
+                }
+                else {
+                    const result = await staffsCollection.updateOne(filter, updateDoc);
+                    res.send(result);
+                }
+            } catch (err) {
+                res.status(500).send({ error: 'Update failed', details: err.message });
+            }
+        });
+        app.put('/additional_movement_submit/:id', async (req, res) => {
+            const id = req.params.id;
+            const bodyData = req.body;
+            const filter = { _id: new ObjectId(id) };
+
+            try {
+                // Update database: Push daily data & reset today's values
+                const updateDoc = {
+                    $set: {
+                        additional_enter_time: '',
+                        additional_exit_time: '',
+                        additional_movement_hour: bodyData.additional_movement_hour,
+                        additional_movement_minute: bodyData.additional_movement_minute
+                    }
+                };
+                const ErrorDoc = {
+                    $set: {
+                        additional_enter_time: '',
+                        additional_exit_time: '',
+                        additional_movement_hour: 0,
+                        additional_movement_minute: 0
+                    }
+                }
+                if (bodyData.additional_enter_time === '') {
+                    const result = await staffsCollection.updateOne(filter, ErrorDoc);
+                    res.send(result);
+                }
+                else {
+                    const result = await staffsCollection.updateOne(filter, updateDoc);
+                    res.send(result);
+                }
+            } catch (err) {
+                res.status(500).send({ error: 'Update failed', details: err.message });
+            }
+        });
+
+
+        app.put('/transection_details/:id', async (req, res) => {
+            const id = req.params.id;
+            const bodyData = req.body;
+            const filter = { _id: new ObjectId(id) };
+
+            const newTransectionsData = {
+                transection_id: bodyData.transection_id,
+                transection_date: bodyData.currentDate,
+                transection_amount: bodyData.transection_amount,
+                transection_type: bodyData.transection_type,
+                comment: bodyData.comment
+            };
+
+            try {
+                const staff = await staffsCollection.findOne(filter);
+
+                // Step 1: If length > 19, remove first transection
+                if (staff?.transections?.length > 19) {
+                    await staffsCollection.updateOne(filter, { $pop: { transections: -1 } }); // remove first
+                }
+
+                // Step 2: Push new transection + update balances
+                const updateDoc = {
+                    $push: {
+                        transections: newTransectionsData
+                    },
+                    $set: {
+                        withdrawal_amount: bodyData.withdrawal_amount,
+                        available_balance: bodyData.available_balance
+                    }
+                };
+
+                const result = await staffsCollection.updateOne(filter, updateDoc);
+                res.send(result);
+
+            } catch (err) {
+                console.error(err);
+                res.status(500).send({ error: 'Update failed', details: err.message });
+            }
+        });
+        app.put('/closing_month/:id', async (req, res) => {
+            const id = req.params.id;
+            const bodyData = req.body;
+            const filter = { _id: new ObjectId(id) };
+
+            const newIncomeHistory = {
+                month_name: bodyData.month_name,
+                total_income: bodyData.total_income,
+                total_worked_time: `${bodyData.total_working_hour} Hour, ${bodyData.total_working_minute} Minute`,
+                paid_amount: bodyData.paid_amount,
+                paid_date: bodyData.paid_date,
+            };
+
+            try {
+                const staff = await staffsCollection.findOne(filter);
+
+                // Step 1: If length > 19, remove first transection
+                if (staff?.income_history?.length > 12) {
+                    await staffsCollection.updateOne(filter, { $pop: { income_history: -1 } }); // remove first
+                }
+
+                // Step 2: Push new transection + update balances
+                const updateDoc = {
+                    $push: {
+                        income_history: newIncomeHistory
+                    },
+                    $set: {
+                        total_income: 0,
+                        total_working_hour: 0,
+                        total_working_minute: 0,
+                        withdrawal_amount: 0,
+                        available_balance: bodyData.last_month_due,
+                        last_month_due: bodyData.last_month_due,
+                        current_month_details: []
+                    }
+                };
+
+                const result = await staffsCollection.updateOne(filter, updateDoc);
+                res.send(result);
+
+            } catch (err) {
+                console.error(err);
+                res.status(500).send({ error: 'Update failed', details: err.message });
+            }
+        });
+
+        app.put('/set_user_category/:uid', async (req, res) => {
+            const uid = req.params.uid;
+            const filter = { uid: uid };
+            const options = { upsert: true };
+            const updated = req.body;
+            const newUserCategory = {
+                $set: {
+                    user_category: updated.user_category
+                }
+            };
+
+            try {
+                const result = await staffsCollection.updateOne(filter, newUserCategory, options);
+                res.send(result);
+            } catch (err) {
+                res.status(500).send({ error: 'Update failed', details: err });
+            }
+        });
+        app.put('/set_user_status/:uid', async (req, res) => {
+            const uid = req.params.uid;
+            const filter = { uid: uid };
+            const options = { upsert: true };
+            const updated = req.body;
+            const newUserCategory = {
+                $set: {
+                    status: updated.status
+                }
+            };
+
+            try {
+                const result = await staffsCollection.updateOne(filter, newUserCategory, options);
+                res.send(result);
+            } catch (err) {
+                res.status(500).send({ error: 'Update failed', details: err });
+            }
+        });
+        app.get('/shop_transections', async (req, res) => {
+            const result = await shopTransectionsCollection.find().toArray();
+            res.send(result);
+        });
+        app.put('/shop_transections', async (req, res) => {
+            const filter = { _id: new ObjectId(process.env.Shop_Transections_ObjectId) }
+            const bodyData = req.body;
+            const transection = {
+                transection_id: bodyData.transection_id,
+                transection_date: bodyData.transection_date,
+                transection_amount: bodyData.transection_amount,
+                transection_explaination: bodyData.transection_explaination
+            }
+            if (bodyData.transection_type === 'revenue') {
+                const updateDoc = {
+                    $push: {
+                        revenue_transections: transection
+                    },
+                    $set: {
+                        total_revenue_amount: bodyData.total_revenue_amount,
+                        hand_on_cash: bodyData.hand_on_cash
+                    }
+                }
+                const result = await shopTransectionsCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }
+            else {
+                const updateDoc = {
+                    $push: {
+                        expense_transections: transection
+                    },
+                    $set: {
+                        total_expense_amount: bodyData.total_expense_amount,
+                        hand_on_cash: bodyData.hand_on_cash
+                    }
+                }
+                const result = await shopTransectionsCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }
+        })
+        app.get('/shop_transections_summary', async (req, res) => {
+            const result = await shopTransectionsSummaryCollection.find().toArray();
+            res.send(result);
+        });
+        app.get('/notice_panel', async (req, res) => {
+            const result = await noticePanelCollection.find().toArray();
+            res.send(result);
+        });
 
 
 
